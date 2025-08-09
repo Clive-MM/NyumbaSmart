0f3d3baa03ec015b71efa4e9130606ae243527c9
Clive Moyia <maderumoyia@gmail.com>
2025-08-09 13:20:57 +0300

    Updated landing page

--- frontend/src/components/LandingPage/Operation.jsx
+++ frontend/src/components/LandingPage/Operation.jsx
@@ -322,7 +322,7 @@ export default function Operation() {
             whileHover={!prefersReduced ? "hover" : "rest"}
             whileTap="tap"
           >
-            Get Started
+            SIGN UP HERE
           </GlowButton>
         </Box>
       </Container>
